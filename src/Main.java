848065287fa8a69e55b2fd016244a14c7b28b93d
Szymon Skazinski <szyskazi@gmail.com>
2020-04-09 15:51:03 +0200

    Added new ContactManager object and some contact objects. Try to search some contact and println phone number of it

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-public class Main {
-    public static void main(String [] args){
-        System.out.println("Hello World!");
+ class Main {
+     public static void main(String[] args) {
+        ContactManager myContactManager = new ContactManager();
 
+        Contact contact1 = new Contact();
+        contact1.name = "Grzegorz";
+        contact1.phone = "123456";
+        myContactManager.addContact(contact1);
+
+         Contact contact2 = new Contact();
+         contact2.name = "Magda";
+         contact2.phone = "128886";
+         myContactManager.addContact(contact2);
+
+         Contact contact3 = new Contact();
+         contact3.name = "Izabela";
+         contact3.phone = "125556";
+         myContactManager.addContact(contact3);
+
+         Contact contactNumber = myContactManager.searchContact("Grzegorz");
+         String phoneNumber = contactNumber.phone;
+         System.out.println(phoneNumber);
+
+
+     }
+ }
         class Contact{
             String name;
             String email;
@@ -21,8 +43,15 @@ public class Main {
                 myFriends[friendsCount] = contact;
                 friendsCount++;
             }
-            
+
+            Contact searchContact(String searchName){
+                for (int i=0; i<friendsCount;i++)
+                {
+                    if (myFriends[i].name.equals(searchName)) {
+                        return myFriends[i];
+                    }
+                }
+                return null;
+            }
         }
 
-    }
-}
